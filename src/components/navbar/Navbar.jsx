7f60fca6b9261ffae1680ade47f3cf1076be7bc0
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-01 15:09:50 +0500

    Navbar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,27 @@ import "./navbar.css";
 
 // BEM -> Block Element Modifier
 
+const Menu = () => (
+	<>
+		<p>
+			<a href="#home">Home</a>
+		</p>
+
+		<p>
+			<a href="#wgpt3">What is GPT3?</a>
+		</p>
+		<p>
+			<a href="#possibility">Open AI</a>
+		</p>
+		<p>
+			<a href="#features">Case Studies</a>
+		</p>
+		<p>
+			<a href="#blog">Library</a>
+		</p>
+	</>
+);
+
 const Navbar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -17,22 +38,7 @@ const Navbar = () => {
 				</div>
 			</div>
 			<div className="gpt3__navbar-links_container">
-				<p>
-					<a href="#home">Home</a>
-				</p>
-
-				<p>
-					<a href="#wgpt3">What is GPT3?</a>
-				</p>
-				<p>
-					<a href="#possibility">Open AI</a>
-				</p>
-				<p>
-					<a href="#features">Case Studies</a>
-				</p>
-				<p>
-					<a href="#blog">Library</a>
-				</p>
+				<Menu />
 			</div>
 			<div className="gpt3__navbar-sign">
 				<p>Sign in</p>
@@ -55,6 +61,18 @@ const Navbar = () => {
 							setToggleMenu(true);
 						}}
 					/>
+				)}
+
+				{toggleMenu && (
+					<div className="gpt3__navbar-menu_container scale-up-center">
+						<div className="gpt3__navbar-menu_container-links">
+							<Menu />
+							<div className="gpt3__navbar-menu_container-links-sign">
+								<p>Sign in</p>
+								<button type="button">Sign up</button>
+							</div>
+						</div>
+					</div>
 				)}
 			</div>
 		</div>
