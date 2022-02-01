87e0cc689458a7efd1efa94f4e22d19a3dd4d620
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-01 14:41:03 +0500

    adding styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,11 +17,24 @@ const Navbar = () => {
 			<div className="gpt3__navbar-links_container">
 				<p>
 					<a href="#home">Home</a>
-					<a href="#wgp3">What is GPT3?</a>
+				</p>
+
+				<p>
+					<a href="#wgpt3">What is GPT3?</a>
+				</p>
+				<p>
 					<a href="#possibility">Open AI</a>
+				</p>
+				<p>
 					<a href="#features">Case Studies</a>
+				</p>
+				<p>
 					<a href="#blog">Library</a>
 				</p>
+			</div>
+			<div className="gpt3__navbar-sign">
+				<p>Sign in</p>
+				<button type="button">Sign up</button>
 			</div>
 		</div>
 	);
