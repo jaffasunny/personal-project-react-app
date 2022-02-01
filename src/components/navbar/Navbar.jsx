45b5405979d5de53f126ce3ac5eb9719f0c08056
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-01 14:48:28 +0500

    adding styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { RiMenu3Line, RiClosein } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 
 import "./navbar.css";
@@ -7,6 +7,8 @@ import "./navbar.css";
 // BEM -> Block Element Modifier
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
 	return (
 		<div className="gpt3__navbar">
 			<div className="gpt3__navbar-links">
@@ -35,6 +37,25 @@ const Navbar = () => {
 			<div className="gpt3__navbar-sign">
 				<p>Sign in</p>
 				<button type="button">Sign up</button>
+			</div>
+			<div className="gpt3__navbar-menu">
+				{toggleMenu ? (
+					<RiCloseLine
+						color="#fff"
+						size={27}
+						onClick={() => {
+							setToggleMenu(false);
+						}}
+					/>
+				) : (
+					<RiMenu3Line
+						color="#fff"
+						size={27}
+						onClick={() => {
+							setToggleMenu(true);
+						}}
+					/>
+				)}
 			</div>
 		</div>
 	);
