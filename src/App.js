62200c5cc29cc4a8e5d988c3629ec734d98af7df
jaffasunny <jaffer.sunny125@gmail.com>
2022-02-01 13:42:21 +0500

    Added Containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+
+// import { Article, Brand, CTA, Feature, Navbar } from "./components";
 
 export const App = () => {
-  return <div>
-      <h1>GPT</h1>
-  </div>;
+	return (
+		<div>
+			<h1>GPT</h1>
+		</div>
+	);
 };
